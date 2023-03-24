63a2aed86704581de3e75dc753d6f98ffa61e979
dhi13man <furyx.ds@gmail.com>
2023-03-24 20:16:19 +0530

    Improved architecture and added VPN always on functionality.

--- __main__.py
+++ __main__.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python3
 
 from threading import Thread
+from sys import argv
 
 from src.models.vpn_data import interface
-from src.models.vpn_data import pritunl_vpn_data
+from src.services.vpn_parser_service import VpnParserService
 
-from sys import argv
+PROMPT: str = "Please enter whether you would like to Connect (c), Disconnect (d), or be in Always-Connect mode (w)"
 
 def get_switch() -> chr:
   if len(argv) < 2:
-    return input("Please enter whether you would like to Connect (c) or Disconnect (d): ").lower()
+    return input(f"{PROMPT}: ").lower()
   else:
     return argv[1].lower()
 
 if __name__ == "__main__":
   # Get and Validate user switch to know whether to connect or disconnect VPNs
   switch: chr = get_switch()
-  if switch not in ["c", "d"]:
-    raise ValueError("Invalid switch. Please enter either 'c' or 'd'")
+  if switch not in ["w", "c", "d"]:
+    raise ValueError(f"Invalid switch. {PROMPT}!'")
 
   # List all VPNs
-  vpn_data_list: list[interface.VpnDataInterface] = [
-    pritunl_vpn_data.PritunlVpnData(id="", pin="", totp_url=""),
-  ]
+  vpn_data_list: list[interface.VpnDataInterface] = []
+  with open("./vpn_data.json", "r") as f:
+    vpn_parser_service: VpnParserService = VpnParserService()
+    vpn_data_list = vpn_parser_service.parse_vpn_data(f.read())
 
-  # Perform action on all VPNs
-  threads: set[Thread] = set()
-  for vpn in vpn_data_list:
-    t: Thread = Thread(target=vpn.connect if switch == 'c' else vpn.disconnect)
-    t.start()
-    threads.add(t)
+  while True:
+    # Perform action on all VPNs
+    threads: set[Thread] = set()
+    for vpn in vpn_data_list:
+      t: Thread = Thread(target=vpn.disconnect if switch == 'd' else vpn.connect)
+      t.start()
+      threads.add(t)
 
-  # Ensure all threads complete before exit
-  for t in threads: t.join()
+    # Ensure all threads complete before exit
+    for t in threads: t.join()
+    
+    # If not in watch mode, exit
+    if switch != 'w': break
     
